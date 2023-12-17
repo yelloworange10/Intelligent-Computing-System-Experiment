0d3862edbe7d8d68d6ad1f02f771a594b7229630
黄成梓 <126266928+yelloworange10@users.noreply.github.com>
2023-12-17 11:42:43 +0800

    Update train-mlu.py

--- chapter4/exp_4_3_fast_style_transfer_infer_student/stu_upload/train-mlu.py
+++ chapter4/exp_4_3_fast_style_transfer_infer_student/stu_upload/train-mlu.py
@@ -8,6 +8,7 @@ import torch_mlu
 import cv2
 import numpy
 import os
+from torch.optim import Adam
 
 os.putenv('MLU_VISIBLE_DEVICES','0')
 
@@ -180,32 +181,33 @@ def get_gram_matrix(f_map):
 if __name__ == '__main__':
     image_style = load_image('./data/udnie.jpg').cpu()
     #TODO: 将输入的风格图像加载到mlu设备上,得到mlu_iamge_style
-    ______________________________________
+    mlu_image_style = image_style.to('mlu')
     net = VGG19().cpu()
     g_net = TransNet().cpu()
     #TODO: 将特征网络加载到mlu得到mlu_g_net
-    ______________________________________
+    mlu_net = net.to('mlu')
     #TODO: 将图像转换网络加载到mlu得到mlu_net
-    ______________________________________
+    mlu_g_net = g_net.to('mlu')
     print("mlu_net build PASS!\n")
     #TODO: 使用adam优化器对mlu_g_net的参数进行优化
-    ______________________________________
+    optimizer = Adam(mlu_g_net.parameters(), lr=0.001)
     #TODO: 在cpu上计算均方误差损失函得到loss_func
-    ______________________________________
+    loss_func = nn.MSELoss().cpu()
     #TODO: 将损失函数加载到mlu上得到mlu_loss_func
-    ______________________________________
+    mlu_loss_func = loss_func.to('mlu')
     print("build loss PASS!\n")
     data_set = COCODataSet()
     print("load COCODataSet PASS!\n")
     batch_size = 1
     data_loader = DataLoader(data_set, batch_size, True, drop_last=True)
     #TODO：mlu_iamge_style经过特征提取网络mlu_net生成风格特征s1-s4
-    ______________________________________
+    s1, s2, s3, s4 = mlu_net(mlu_image_style)
     #TODO: 对风格特征s1-s4计算格拉姆矩阵并从当前计算图中分离下来，得到对应的s1-s4
-    ______________________________________
-    ______________________________________
-    ______________________________________
-    ______________________________________
+    s1 = get_gram_matrix(s1).detach()
+    s2 = get_gram_matrix(s2).detach()
+    s3 = get_gram_matrix(s3).detach()
+    s4 = get_gram_matrix(s4).detach()
+
     j = 0
     count = 0
     epochs = 100
@@ -213,26 +215,26 @@ if __name__ == '__main__':
         for i, image in enumerate(data_loader):
             image_c = image.cpu()
             #TODO: 将输入图像拷贝到mlu上得到mlu_image_c
-            ______________________________________
+            mlu_image_c = image_c.to('mlu')
             #TODO: 将mlu_image_c经过mlu_g_net输出生成图像mlu_imge_g
-            ______________________________________
+            mlu_image_g = mlu_g_net(mlu_image_c)
             #TODO: 利用特征提取网络mlu_net提取生成图像mlu_image_g的特征out1-out4
-            ______________________________________
+            out1, out2, out3, out4 = mlu_net(mlu_image_g)
             ##############计算风格损失#################
             #TODO: 对生成图像的特征out1-out4计算gram矩阵，并与风格图像的特征求损失，分别得到loss_s1-loss_s4
-            ______________________________________
-            ______________________________________
-            ______________________________________
-            ______________________________________
+            loss_s1 = mlu_loss_func(get_gram_matrix(out1), s1)
+            loss_s2 = mlu_loss_func(get_gram_matrix(out2), s2)
+            loss_s3 = mlu_loss_func(get_gram_matrix(out3), s3)
+            loss_s4 = mlu_loss_func(get_gram_matrix(out4), s4)
             #TODO：loss_s1-loss_s4相加得到风格损失loss_s
-            ______________________________________
+            loss_s = loss_s1 + loss_s2 + loss_s3 + loss_s4
 
             ##############计算内容损失#################
             #TODO: 将图片mlu_image_c经过特征提取网络mlu_net得到内容特图像的特征c1-c4
-            ______________________________________
+            c1, c2, c3, c4 = mlu_net(mlu_image_c)
 
-             #TODO: 将内容图像特征c2从计算图中分离并与内容图像特征out2求内容损失loss_c2
-            ______________________________________
+            #TODO: 将内容图像特征c2从计算图中分离并与内容图像特征out2求内容损失loss_c2
+            loss_c2 = mlu_loss_func(out2, c2.detach())
             loss_c = loss_c2
 
             ##############计算总损失#################
@@ -240,21 +242,21 @@ if __name__ == '__main__':
 
             ########清空梯度、计算梯度、更新参数######
             #TODO: 梯度初始化为零
-            ______________________________________
+            optimizer.zero_grad()
             #TODO: 反向传播求梯度
-            ______________________________________
+            loss.backward()
             #TODO: 更新所有参数
-            ______________________________________
+            optimizer.step()
             print('j:',j, 'i:',i, 'loss:',loss.item(), 'loss_c:',loss_c.item(), 'loss_s:',loss_s.item())
             count += 1
             mlu_image_g = mlu_image_g.cpu()
             mlu_image_c = mlu_image_c.cpu()
             if i % 10 == 0:
                 #TODO: 将图像转换网络fst_train_mlu.pth的参数存储在models/文件夹下
-                ______________________________________
+                torch.save(g_net.state_dict(), './models/fst_train_mlu.pth')
                 #TODO: 利用save_image函数将tensor形式的生成图像mlu_image_g以及输入图像mlu_image_c以jpg左右拼接的形式保存在/out/train_mlu/文件夹下
-                ______________________________________
-        j += 1
+                save_image(torch.cat((mlu_image_c, mlu_image_g), 2), f'./out/train_mlu/train_result_{j}_{i}.jpg')
+    j += 1
 
 print("MLU TRAIN RESULT PASS!\n")
 
